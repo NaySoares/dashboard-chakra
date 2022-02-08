6a6043d23261a70ac0a80e3ae5b98373c5599c06
Elienai <elienay.soares07@gmail.com>
2022-02-08 18:15:15 -0300

    update(*): resized mobile

--- src/pages/users/create.tsx
+++ src/pages/users/create.tsx
@@ -1,8 +1,6 @@
-import { Box, Flex, Heading, VStack, HStack, SimpleGrid, Button, Icon, Table, Tr, Th, Td, Tbody, Thead, Checkbox, Text, Divider} from "@chakra-ui/react";
-import { RiAddLine, RiPencilLine } from "react-icons/ri";
+import { Box, Flex, Heading, VStack, HStack, SimpleGrid, Button, Divider} from "@chakra-ui/react";
 import { Input } from "../../components/Form/Input";
 import { Header } from '../../components/Header';
-import { Pagination } from "../../components/Pagination";
 import { Sidebar } from "../../components/Sidebar";
 
 export default function CreateUser() {
@@ -18,24 +16,24 @@ export default function CreateUser() {
       >
         <Sidebar/>
 
-        <Box flex="1" borderRadius="8" bg="gray.800" p="8">
+        <Box flex="1" borderRadius="8" bg="gray.800" p={["6","8"]}>
           <Heading size="lg" fontWeight="normal">Criar Usuário</Heading>
           
           <Divider my="6" borderColor="gray.700"></Divider>
 
-          <VStack spacing="8">
-            <SimpleGrid minChildWidth="240px" spacing="8" w="100%">
+          <VStack spacing={["6","8"]}>
+            <SimpleGrid minChildWidth="240px" spacing={["6","8"]} w="100%">
               <Input name="name" label="Nome Completo"/>
               <Input name="email" type="email" label="E-mail"/>
             </SimpleGrid>
 
-            <SimpleGrid minChildWidth="240px" spacing="8" w="100%">
+            <SimpleGrid minChildWidth="240px" spacing={["6","8"]} w="100%">
               <Input name="password" type="password" label="Senha"/>
               <Input name="password_confirmation" type="password" label="Confirmação da Senha"/>
             </SimpleGrid>
           </VStack>
 
-          <Flex mt="8" justify="flex-end">
+          <Flex mt={["6","8"]} justify="flex-end">
             <HStack spacing="4">
               <Button colorScheme="whiteAlpha">Cancelar</Button>
               <Button colorScheme="pink">Salvar</Button>
--- src/pages/users/index.tsx
+++ src/pages/users/index.tsx
@@ -1,67 +1,85 @@
-import { Box, Flex, Heading, Button, Icon, Table, Tr, Th, Td, Tbody, Thead, Checkbox, Text} from "@chakra-ui/react";
+import {
+  Box,
+  Flex,
+  Heading,
+  Button,
+  Icon,
+  Table,
+  Tr,
+  Th,
+  Td,
+  Tbody,
+  Thead,
+  Checkbox,
+  Text,
+  useBreakpointValue,
+} from "@chakra-ui/react";
 import { RiAddLine, RiPencilLine } from "react-icons/ri";
 import { Header } from "../../components/Header";
-import { Pagination } from "../../components/Pagination";
+import { Pagination } from "../../components/Pagination/Index";
 import { Sidebar } from "../../components/Sidebar";
 
 export default function UserList() {
-  return(
+  const isWideVersion = useBreakpointValue({
+    base: false,
+    lg: true
+  })
+
+  return (
     <Box>
       <Header />
 
-      <Flex 
-        w="100%"
-        maxWidth={1480}
-        mx="auto"
-        px="6"
-      >
-        <Sidebar/>
+      <Flex w="100%" maxWidth={1480} mx="auto" px="6">
+        <Sidebar />
 
         <Box flex="1" borderRadius="8" bg="gray.800" p="8">
           <Flex mb="8" justify="space-between" align="center">
-            <Heading size="lg" fontWeight="normal">Usuários</Heading>
+            <Heading size="lg" fontWeight="normal">
+              Usuários
+            </Heading>
             <Button
               as="a"
-              size="sm" 
+              size="sm"
               fontWeight="sm"
               colorScheme="pink"
-              leftIcon={ <Icon as={RiAddLine} fontSize="18"/>}
+              leftIcon={<Icon as={RiAddLine} fontSize="18" />}
             >
-            Criar Novo
+              Criar Novo
             </Button>
-
           </Flex>
 
           <Table colorScheme="whiteAlpha">
             <Thead>
               <Tr>
-                <Th px="6" color="gray.300" width="8">
+                <Th px={["4", "4", "6"]} color="gray.300" width="8">
                   <Checkbox colorScheme="pink" />
                 </Th>
                 <Th>Usuários</Th>
-                <Th>Data de Cadastro</Th>
+                { isWideVersion && <Th>Data de Cadastro</Th> }
                 <Th width="8"></Th>
               </Tr>
             </Thead>
             <Tbody>
               <Tr>
-                <Td px="6">
+                <Td px={["4", "4", "6"]}>
                   <Checkbox colorScheme="pink" />
                 </Td>
                 <Td>
                   <Box>
                     <Text fontWeight="bold">Elienai Soares</Text>
-                    <Text fontSize="sm" color="gray.300">elienay.saores07@gmail.com</Text>
+                    <Text fontSize="sm" color="gray.300">
+                      elienay.saores07@gmail.com
+                    </Text>
                   </Box>
                 </Td>
-                <Td>16 de Novembro de 2021</Td>
+                { isWideVersion && <Td>16 de Novembro de 2021</Td> }
                 <Td>
                   <Button
                     as="a"
-                    size="sm" 
+                    size="sm"
                     fontWeight="sm"
                     colorScheme="pink"
-                    leftIcon={ <Icon as={RiPencilLine} fontSize="16"/>}
+                    leftIcon={<Icon as={RiPencilLine} fontSize="16" />}
                   >
                     Editar
                   </Button>
@@ -74,5 +92,5 @@ export default function UserList() {
         </Box>
       </Flex>
     </Box>
-  )
+  );
 }
