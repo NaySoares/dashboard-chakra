611334a9e26d4db7fa2a90580311ec02c36ba8c8
Elienai <elienay.soares07@gmail.com>
2022-02-10 16:27:35 -0300

    update(mirage): set activeModelSeralizer

--- src/services/mirage/index.ts
+++ src/services/mirage/index.ts
@@ -1,4 +1,4 @@
-import { createServer, Factory, Model, Response } from 'miragejs';
+import { createServer, Factory, Model, Response, ActiveModelSerializer } from 'miragejs';
 import faker from '@faker-js/faker';
 
 type User = {
@@ -9,13 +9,17 @@ type User = {
 
 export function makeServer() {
   const server = createServer({
+    serializers: {
+      application: ActiveModelSerializer 
+    },
+
     models: {
       user: Model.extend<Partial<User>>({})
     },
 
     factories: {
       user: Factory.extend({
-        name(i: number) {
+        name() {
           return `${faker.name.firstName()} ${faker.name.lastName()}`
         },
         email() {
